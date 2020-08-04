60c1adba93d6dbe6fd96af8f94c675c012ddec14
전준엽 <galid1@naver.com>
2020-08-04 12:29:25 +0900

    mod
    - 로그인 회원가입 처리를 위해서, KakaoLoginController 의 이름을 추상적인 KakaoController로 변경
    - Controller에서 처리하던 로직을 KakaApiClient로 빼냄

---
+++ src/main/java/com/galid/social_logins/socials/kakao/KakaoController.java
@@ -0,0 +1,25 @@
+package com.galid.social_logins.socials.kakao;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.IOException;
+
+@Controller
+@RequiredArgsConstructor
+public class KakaoController {
+    private final ObjectMapper objMapper;
+    private final KakaoApiClient kakaoApiClient;
+
+    @GetMapping("/auth/kakao/code")
+    public String getAccessToken(@RequestParam("code") String authorizeCode) throws IOException {
+        System.out.println("kakao : " + kakaoApiClient.getUniqueId(authorizeCode));
+
+        return "main";
+    }
+
+
+}
