171e3f23bbccb96b7fb09d7342e26033b7fb3ee4
전준엽 <galid1@naver.com>
2020-08-04 12:29:50 +0900

    add Social Api url들을 모은 상수 클래스 추가

---
+++ src/main/java/com/galid/social_logins/socials/commons/ApiBaseUrls.java
@@ -0,0 +1,12 @@
+package com.galid.social_logins.socials.commons;
+
+public class ApiBaseUrls {
+    public static String KAKAO_ACCESSTOKEN_BASE_URL = "https://kauth.kakao.com";
+    public static String KAKAO_INFORMATION_BASE_URL = "https://kapi.kakao.com";
+
+    public static String NAVER_ACCESSTOKEN_BASE_URL = "https://nid.naver.com/oauth2.0/token";
+    public static String NAVER_INFORMATION_BASE_URL = "https://openapi.naver.com/v1/nid/me";
+
+    public static String GOOGLE_ACCESSTOKEN_BASE_URL = "https://oauth2.googleapis.com/tokeninfo";
+    public static String GOOGLE_INFORMATION_BASE_URL = "";
+}
