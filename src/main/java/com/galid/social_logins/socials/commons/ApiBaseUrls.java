56cfac22a662daad2dc0e10d4b9f7341e58b2d8e
전준엽 <galid1@naver.com>
2020-08-06 22:48:59 +0900

    mod oauth 구글 root url 변경

--- src/main/java/com/galid/social_logins/socials/commons/ApiBaseUrls.java
+++ src/main/java/com/galid/social_logins/socials/commons/ApiBaseUrls.java
@@ -7,6 +7,6 @@ public class ApiBaseUrls {
     public static String NAVER_ACCESSTOKEN_BASE_URL = "https://nid.naver.com";
     public static String NAVER_INFORMATION_BASE_URL = "https://openapi.naver.com";
 
-    public static String GOOGLE_ACCESSTOKEN_BASE_URL = "https://oauth2.googleapis.com";
-    public static String GOOGLE_INFORMATION_BASE_URL = "";
+    public static String GOOGLE_ACCESSTOKEN_BASE_URL = "";
+    public static String GOOGLE_INFORMATION_BASE_URL = "https://oauth2.googleapis.com";
 }
