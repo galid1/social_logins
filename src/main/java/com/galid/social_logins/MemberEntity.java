48c4db76293340aca587df6de87a028e5f0f760d
전준엽 <galid1@naver.com>
2020-08-08 22:09:44 +0900

    mod MemberEntity 멤버 변수 변경
    - authId 대신 socialId로 변경하여 가독성 향상
    - 사용하지 않는 email 제거

--- src/main/java/com/galid/social_logins/MemberEntity.java
+++ src/main/java/com/galid/social_logins/MemberEntity.java
@@ -4,10 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "member")
@@ -17,12 +14,13 @@ public class MemberEntity {
     @Id @GeneratedValue
     private Long memberId;
 
-    private String email;
-    private String authId;
+    private String socialId;
+    @Enumerated(value = EnumType.STRING)
+    private SocialType socialType;
 
     @Builder
-    public MemberEntity(String email, String authId) {
-        this.email = email;
-        this.authId = authId;
+    public MemberEntity(String socialId, SocialType socialType) {
+        this.socialId = socialId;
+        this.socialType = socialType;
     }
 }
