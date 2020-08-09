04b0ce16cd086acc56777a5e3a8df50f1c3e854a
전준엽 <galid1@naver.com>
2020-08-09 18:19:16 +0900

    mod 회원가입 서비스 사용하도록 변경

--- src/main/java/com/galid/social_logins/MainController.java
+++ src/main/java/com/galid/social_logins/MainController.java
@@ -1,23 +1,37 @@
 package com.galid.social_logins;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigInteger;
 import java.security.SecureRandom;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+    private final MemberService memberService;
+
     @GetMapping("/join")
-    public String getJoinPage() {
+    public String getJoinPage(Model model) {
+        model.addAttribute("csrfToken", generateToken());
         return "join";
+    }
+
+    @GetMapping("/doJoin")
+    public String join(@ModelAttribute MemberJoinRequest request) {
+        memberService.signUp(request);
+        return "main";
     }
 
     @GetMapping("/login")
     public String getLoginPage(Model model) {
         model.addAttribute("csrfToken", generateToken());
-        return "login";
+        return "join";
     }
 
     private String generateToken() {
