70d0f8c6b3ced3e715b8acdd8c67b78d35479ded
전준엽 <galid1@naver.com>
2020-08-14 13:48:55 +0900

    mod 회원가입 로직 완성

--- src/main/java/com/galid/social_logins/MemberService.java
+++ src/main/java/com/galid/social_logins/MemberService.java
@@ -10,8 +10,16 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public Long signUp() {
+    public Long signUp(MemberJoinRequest joinRequest) {
+        validateDuplicateId(joinRequest.getSocialId());
 
-        return 0l;
+        MemberEntity entity = memberRepository.save(joinRequest.toEntity());
+        return entity.getMemberId();
+    }
+
+    private void validateDuplicateId(String socialId) {
+        if(memberRepository.findFirstBySocialId(socialId)
+            .isPresent())
+            throw new IllegalStateException("이미 가입된 상태입니다.");
     }
 }
